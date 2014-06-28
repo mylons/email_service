63b786e09628d03a86409b472b4091092b1002d0
Michael Lyons <mrlyons@gmail.com>
2014-06-28 11:05:35 -0700

    project skeleton updated. can now accept post requests, validates json

--- service.py
+++ service.py
@@ -1,8 +1,19 @@
 from flask import Flask
+from flask import request
+
+from util import validator
+from util.validator import ValidationError
+
 # get a flask instance
 app = Flask(__name__)
+# pre cache some objects/lists
+json_keys = ['to', 'to_name', 'from', 'from_name', 'subject', 'body']
+json_validator = validator.JSON()
+str_validator = validator.StringLength(the_min=2)
+email_validator = validator.Email()
 
-@app.route('/email')
+
+@app.route('/email', methods=['POST'])
 def email():
     """
     json spec (k: v):
@@ -15,12 +26,40 @@ def email():
 
     all fields required
 
-    :return:
+    only accepts POST requests
+    only operates on JSON data
     """
-    # get json
+    app.logger.debug(request.json)
+    if valid(request.json):
+        app.logger.debug("json is valid")
+        send_email(request.json)
+    else:
+        app.logger.debug("json not valid")
+    return "lawl"
 
-    # validate json
 
-    #
-    return '<p> email sent </p>'
+def valid(the_json):
+    #validate -- return true/false
+    # these call functions throw exceptions
+    try:
+        # validate required json keys are there
+        json_validator(json_keys, the_json)
+        # validate those fields have strings with some content
+        for k in json_keys:
+            app.logger.debug("validating %s: %s" % (k, the_json[k]))
+            str_validator(k, the_json[k])
+        # validate email format
+        email_validator('to', the_json['to'])
+        email_validator('from', the_json['from'])
 
+    except ValidationError:
+        return False
+
+    return True
+
+def send_email(the_json):
+    # send the email out
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
